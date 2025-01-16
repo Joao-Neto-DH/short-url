9bfff9a4172f2efdefc15609cc7be4c24fb1bd3b
João Neto <joaolima882018@gmail.com>
2025-01-16 16:45:49 +0100

    feat: layout principal

--- src/app/(shorter)/short/page.tsx
+++ src/app/(shorter)/short/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import FormGenerateShort from "./form-generate-short";
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <div className="w-full">
+      <div className="py-3 px-6 border-b">
+        <div className="flex flex-row items-center">
+          <div className="ml-auto">
+            <FormGenerateShort />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
