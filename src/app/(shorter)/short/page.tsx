7d4d3504eef147a21d3f5a804aa420b1b8c59906
João Neto <joaolima882018@gmail.com>
2025-01-17 10:52:48 +0100

    feat: 404 page

--- src/app/(shorter)/short/page.tsx
+++ src/app/(shorter)/short/page.tsx
@@ -13,6 +13,19 @@ function Page() {
           </div>
         </div>
       </div>
+      <div className="max-w-5xl mx-auto p-6">
+        <div className="w-full flex flex-row overflow-x-auto pb-4 space-x-4">
+          {[...new Array(5)].map((_, index) => (
+            <div
+              key={index}
+              className="border border-l-4 border-l-blue-600 rounded-md p-4 min-w-52 shadow-md"
+            >
+              <p className="font-bold text-xl">URLs geradas</p>
+              <p className="font-light text-2xl">12</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import { Input } from "@/components/ui/input";
-
 export default function Home() {
   return (
     <div className="w-full h-full overflow-auto">
       <div className="absolute top-1/2 left-1/2">
-        <form action="#">
-          <Input type="text" />
-        </form>
+        <form action="#"></form>
       </div>
     </div>
   );
