b5d1e0858610200bc53625861c90684ec4125044
João Neto <joaolima882018@gmail.com>
2025-01-17 18:31:23 +0100

    feat: services

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,3 +1,3 @@
 import { PrismaClient } from "../../prisma/generated/client";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
---
+++ src/services/index.tsx
@@ -0,0 +1,88 @@
+import { prisma } from "@/prisma";
+import { Link } from "../../prisma/generated/client";
+
+export default class Services {
+  public async gerarLink(data: Link) {
+    const result = await prisma.link.create({
+      data: {
+        ...data,
+        ContarAcesso: {
+          create: {
+            desktop: 0,
+            mobile: 0,
+          },
+        },
+      },
+      include: { ContarAcesso: true },
+    });
+
+    return result;
+  }
+
+  public async listarLinks(data: Partial<Link>) {
+    const result = await prisma.link.findMany({
+      where: data,
+      include: { ContarAcesso: true },
+    });
+
+    return result;
+  }
+
+  public async verLink(id: number) {
+    const result = await prisma.link.findFirst({
+      where: { id },
+      include: { ContarAcesso: true },
+    });
+
+    return result;
+  }
+
+  public async actualizarLink(id: number, data: Partial<Link>) {
+    const result = await prisma.link.update({
+      where: { id },
+      data: {
+        ...data,
+        id: undefined,
+      },
+      include: { ContarAcesso: true },
+    });
+
+    return result;
+  }
+
+  public async removerLink(id: number) {
+    const result = await prisma.link.delete({
+      where: { id },
+      include: { ContarAcesso: true },
+    });
+
+    return result;
+  }
+
+  public async contarAccesso(
+    palavraChave: string,
+    dispositivo: "PC" | "MOVEL"
+  ) {
+    const access = await prisma.contarAcesso.findFirst({
+      where: {
+        link: {
+          palavra_chave: {
+            equals: palavraChave,
+          },
+        },
+      },
+    });
+
+    const result = await prisma.contarAcesso.update({
+      where: {
+        id: access?.id,
+      },
+      data: {
+        desktop: dispositivo === "PC" ? (access?.desktop ?? 0) + 1 : undefined,
+        mobile: dispositivo === "MOVEL" ? (access?.mobile ?? 0) + 1 : undefined,
+      },
+    });
+
+    return result;
+  }
+}
